fc47ef4eeb3edae1b55553ab15f43bf1b8034295
ljakkob <larissa.jacob01@gmail.com>
2017-08-26 23:41:01 -0300

    Importando arquivos de um servidor externo
    funcionandooo!!!

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { MovieProvider } from "../../providers/movie/movie";
 
 /**
  * Generated class for the FeedPage page.
@@ -12,10 +13,32 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-feed',
   templateUrl: 'feed.html',
+  providers: [
+    MovieProvider
+  ]
 })
 export class FeedPage {
+
+  public objeto_feed = {
+
+    titulo: "Lari",
+    data: "aug 26",
+    descricao:"Olha o q eu fiz",
+    qntd_likes: "125",
+    qnt_comments: "36",
+    time_comment: "15h ago"
+
+
+    
+  }
+
+  public lista_filmes = new Array<any>();
+
+
   public nome_usuario:string="Lari Devona";
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private MovieProvider: MovieProvider) {
+    
+  
   }
  
   public somaDoisNumeros (num1:number, num2:number): void{
@@ -24,7 +47,17 @@ export class FeedPage {
 
 
   ionViewDidLoad() {
-   // this.somaDoisNumeros(10,95);
+   this.MovieProvider.getLatestMovies().subscribe(
+   
+   data=>{
+    const response = (data as any);
+    const objeto_retorno = JSON.parse(response._body);
+    this.lista_filmes = objeto_retorno.results;
+    console.log(objeto_retorno);
+   }, error => {
+     console.log(error);
+   }
+  )
 
   }
 
